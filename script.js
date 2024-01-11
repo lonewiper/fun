6a64e3adea3b1444089cc241963b432783f80712
anantharaman12 <113843469+anantharaman12@users.noreply.github.com>
2024-01-11 19:14:08 +0530

    optimized alert

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
     var hoverCount = 1;
     button.addEventListener('mouseover', function () {
       hoverCount++;
-      if (hoverCount > 1) {
+      if (hoverCount > 3) {
         alert('you stupid panne ');
       }
     });
